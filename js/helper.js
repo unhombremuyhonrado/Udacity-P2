243457facb7b9fa3fb082a2e3447a5f0c8ca372c
unhombremuyhonrado <unhombremuyhonrado@yahoo.com>
2015-01-13 15:16:24 -0500

    Project 2
    Interactive-Resume

---
+++ js/helper.js
@@ -0,0 +1,313 @@
+var HTMLheaderName = "<h1 id='name'>%data%</h1>";
+
+var HTMLheaderRole = "<span>%data%</span><hr/>";
+
+
+var HTMLcontactGeneric = "<li class='flex-item'><span class='orange-text'>%contact%</span><span class='white-text'>%data%</span></li>";
+
+var HTMLmobile = "<li class='flex-item'><span class='orange-text'>mobile</span><span class='white-text'>%data%</span></li>";
+
+var HTMLemail = "<li class='flex-item'><span class='orange-text'>email</span><span class='white-text'>%data%</span></li>";
+
+var HTMLtwitter = "<li class='flex-item'><span class='orange-text'>twitter</span><span class='white-text'>%data%</span></li>";
+
+var HTMLgithub = "<li class='flex-item'><span class='orange-text'>github</span><span class='white-text'>%data%</span></li>";
+
+var HTMLblog = "<li class='flex-item'><span class='orange-text'>blog</span><span class='white-text'>%data%</span></li>";
+
+var HTMLlocation = "<li class='flex-item'><span class='orange-text'>location</span><span class='white-text'>%data%</span></li>";
+
+
+var HTMLbioPic = "<img src='%data%' class='biopic'>";
+var HTMLWelcomeMsg = "<span class='welcome-message'>%data%</span>";
+
+
+var HTMLskillsStart = "<h3 id='skillsH3'>Skills at a Glance:</h3><ul id='skills' class='flex-box'></ul>";
+
+var HTMLskills = "<class='flex-item'><span class='white-text'>%data%</span>";
+
+
+
+//the var HTMLskills was changed to "<class='flex-item'><span class='white-text'>%data%</span>"; FROM:
+//"<li class='flex-item'><span class='white-text'>%data%</span></li>"; 
+
+
+
+
+var HTMLworkStart = "<div class='work-entry'></div>";
+
+var HTMLworkEmployer = "<a href='#'>%data%";
+
+var HTMLworkTitle = " - %data%</a>";
+
+var HTMLworkDates = "<div class='date-text'>%data%</div>";
+
+var HTMLworkLocation = "<div class='location-text'>%data%</div>";
+
+var HTMLworkDescription = "<p><br>%data%</p>";
+
+
+
+var HTMLprojectStart = "<div class='project-entry'></div>";
+
+var HTMLprojectTitle = "<a href='#'>%data%</a>";
+
+var HTMLprojectDates = "<div class='date-text'>%data%</div>";
+
+var HTMLprojectDescription = "<p><br>%data%</p>";
+
+var HTMLprojectImage = "<img src='%data%'>";
+
+
+
+var HTMLschoolStart = "<div class='education-entry'></div>";
+
+var HTMLschoolName = "<a href='#'>%data%";
+
+var HTMLschoolDegree = " -- %data%</a>";
+
+var HTMLschoolDates = "<div class='date-text'>%data%</div>";
+
+var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
+
+var HTMLschoolMajor = "<em><br>Major: %data%</em>"
+
+
+
+var HTMLonlineStart = "<div class='online-entry'></div>"
+
+var HTMLonlineClasses = "<h2>Online Classes</h2>";
+
+var HTMLonlineTitle = "<a href='#'>%data%";
+var HTMLonlineSchool ="<a href='#' class='onlineschool'>%data%";
+
+var HTMLonlineDates = "<div class='date-text'>%data%</div><br>";
+
+var HTMLonlineURL = "<a href='#'>%data%</a>";
+
+
+
+var internationalizeButton = "<button>Internationalize</button>";
+
+var googleMap = "<div id='map'></div>";
+
+
+
+$(document).ready(function() {
+
+  $('button').click(function() {
+
+    var iName = inName() || function(){};
+
+    $('#name').html(iName);
+  
+  });
+
+})
+
+
+
+clickLocations = [];
+
+
+
+function logClicks(x,y) {
+
+  clickLocations.push(
+
+    {
+
+      "x": x,
+
+      "y": y
+
+    }
+
+  );
+
+  console.log("x location: " + x + "; y location: " + y);
+
+}
+
+
+
+$(document).click(function(loc) {
+});
+
+
+
+var map;
+
+
+
+function initializeMap() {
+
+
+  var locations;
+
+  var mapOptions = {
+
+    disableDefaultUI: true
+
+  };
+
+
+
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+  
+
+function locationFinder() {
+
+    var locations = [];
+
+
+
+    locations.push(bio.contacts.location);
+
+
+
+    for (var school in education.schools) {
+
+      locations.push(education.schools[school].location);
+
+    }
+
+
+
+    for (var job in work.jobs) {
+
+      locations.push(work.jobs[job].location);
+
+    }
+
+
+    return locations;
+
+  }
+
+
+
+  function createMapMarker(placeData) {
+
+
+    var lat = placeData.geometry.location.k;
+    var lon = placeData.geometry.location.B;
+  
+  var name = placeData.formatted_address;
+   
+ var bounds = window.mapBounds;
+
+    var marker = new google.maps.Marker({
+
+      map: map,
+
+      position: placeData.geometry.location,
+
+      title: name
+
+    });
+
+
+
+    var infoWindow = new google.maps.InfoWindow({
+
+      content: name
+
+    });
+
+
+
+    google.maps.event.addListener(marker, 'click', function() {
+
+       infoWindow.open(map, marker);
+ 
+
+    });
+
+
+
+    bounds.extend(new google.maps.LatLng(lat, lon));
+
+    map.fitBounds(bounds);
+
+    map.setCenter(bounds.getCenter());
+
+  }
+
+  
+    function callback(results, status) {
+
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+
+      createMapMarker(results[0])
+
+    }
+
+  }
+
+
+  function pinPoster(locations) {
+
+
+    var service = new google.maps.places.PlacesService(map);
+
+        for (place in locations) {
+
+
+
+      var request = {
+
+        query: locations[place]
+
+      }
+
+
+
+      service.textSearch(request, callback);
+
+    }
+
+  }
+
+
+
+  window.mapBounds = new google.maps.LatLngBounds();
+
+
+  locations = locationFinder();
+
+
+  pinPoster(locations);
+
+  
+};
+
+
+window.addEventListener('load', initializeMap);
+
+window.addEventListener('resize', function(e){
+     map.fitBounds(mapBounds);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/resumeBuilder.js
@@ -0,0 +1,295 @@
+var bio = {
+    "name": "Brent Andrew Dare",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile":"865-771-1378",
+        "email":"brent.dare@gmail.com",
+        "location":"Knoxville, TN",
+        "twitter":"brentdare",
+        "github": "unhombremuyhonrado"
+        
+    },
+    "welcomeMessage": "If it doesn't connect to the internet, then I probably shouldn't be touching it.",
+    "skills": [
+        "HTML","CSS", "JavaScript","Python","Java", "PERL","SQL","C#","C++","R","C"
+    ],
+    "bioPic":"images/me.jpg"
+}
+
+
+var formattedName=HTMLheaderName.replace("%data%",bio.name);
+var formattedRole=HTMLheaderRole.replace("%data%",bio.role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var mobile=bio.contacts.mobile;
+var formattedMobile=HTMLformattedMobile=HTMLmobile.replace("%data%",mobile);
+$("#topContacts").append(formattedMobile);
+
+var email=bio.contacts.email;
+var formattedEmail=HTMLemail.replace("%data%",email);
+$("#topContacts").append(formattedEmail);
+
+var github=bio.contacts.github;
+var formattedGit=HTMLgithub.replace("%data%",github);
+$("#topContacts").append(formattedGit);
+
+var bioPic = "images/me.jpg"
+var formattedbioPic = HTMLbioPic.replace("%data%", bioPic);
+$("#header").append(formattedbioPic);
+
+var twitter = bio.contacts.twitter;
+var formattedTwitter = HTMLtwitter.replace("%data%",twitter);
+$("#topContacts").append(formattedTwitter);
+
+var _location = bio.contacts.location;
+var formattedLocation = HTMLlocation.replace("%data%", _location);
+$("#topContacts").append(formattedLocation);
+
+var welcomeMessage = bio.welcomeMessage;
+var formattedwelcomeMessage = HTMLWelcomeMsg.replace("%data%",welcomeMessage);
+$("#header").append(formattedwelcomeMessage);
+
+
+if(bio.skills.length > 0){
+    $("#header").append(HTMLskillsStart);
+
+    var formattedSkill=HTMLskills.replace("%data%",bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[1]);
+    $("#skills").append(formattedSkill);
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[2]);
+    $("#skills").append(formattedSkill);
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[3]);
+    $("#skills").append(formattedSkill);
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[4]);
+    $("#skills").append(formattedSkill)
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[5]);
+    $("#skills").append(formattedSkill)
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[6]);
+    $("#skills").append(formattedSkill)
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[7]);
+    $("#skills").append(formattedSkill)
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[8]);
+    $("#skills").append(formattedSkill)
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[9]);
+    $("#skills").append(formattedSkill)
+    formattedSkill=HTMLskills.replace("%data%",bio.skills[10]);
+    $("#skills").append(formattedSkill)
+
+}
+
+
+
+
+
+var education={
+    "schools":[
+    {
+            "name":"University of Tennessee",
+            "location":"Knoxville",
+            "degree":"Bachelor of Arts",
+            "major":"Spanish",
+            "dates": "2000 - 2005",
+            "url":"http://www.utk.edu"
+    },
+    {
+            "name":"Universidad de Salamanca",
+            "location":"Salamanca, Spain",
+            "degree":"Study Abroad",
+            "major":"Spanish & International Business",
+            "dates": "2004",
+            "url":"http://www.usal.es/webusal/en"
+
+    }
+    ],
+    "onlineCourses":[
+        {
+            "title":"Build a Website from Scratch with HTML & CSS",
+            "school":"Udemy",
+            "dates":"2014",
+            "courseUrl":"https://www.udemy.com/build-website-scratch/",
+            "url":"https://www.udemy.com"
+        },
+        {
+            "title":"Intro to HTML and CSS",
+            "school":"Udacity",
+            "dates":"2014",
+            "courseUrl":"https://www.udacity.com/course/ud304",
+            "url":"https://www.udacity.com"
+        },
+        {
+            "title":"Front End Web Developer Nanodegree",
+            "school":"Udacity",
+            "dates":"2014",
+            "courseUrl":"https://www.udacity.com/course/nd001",
+            "url":"https://www.udacity.com"
+        },
+        {
+            "title":"JavaScript Basics",
+            "school":"Udacity",
+            "dates":"2014",
+            "courseUrl":"https://www.udacity.com/course/ud804",
+            "url":"https://www.udacity.com"
+        },
+        {
+            "title":"PSD to HTML5/CSS3: Hand Code a Beautiful Website in 4-hours!",
+            "school":"Udemy",
+            "dates":"2014",
+            "courseUrl":"https://www.udemy.com/build-beautiful-html5-website/",
+            "url":"https://www.udemy.com"
+        }
+    ]
+}
+
+
+education.display = function(){
+
+    for (school in education.schools){
+        $("#education").append(HTMLschoolStart);
+
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name).replace("#", education.schools[school].url);
+        $(".education-entry:last").append(formattedSchoolName);
+        var formattedschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        $(".education-entry:last").append(formattedschoolDates);
+        var formattedschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+        $(".education-entry:last").append(formattedschoolMajor);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        $(".education-entry:last").append(formattedSchoolLocation);
+        var formattedschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        $(".education-entry:last").append(formattedschoolDegree);
+      };
+
+    for (course in education.onlineCourses) {
+        $("#online").append(HTMLonlineStart);
+
+        var formattedonlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourses[course].title).replace("#", education.onlineCourses[course].courseUrl);
+        $(".online-entry:last").append(formattedonlineTitle);
+
+        var formattedonlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourses[course].school).replace("#", education.onlineCourses[course].url);
+        $(".online-entry:last").append(formattedonlineSchool);
+
+        var formattedonlineDate = HTMLonlineDates.replace("%data%",education.onlineCourses[course].dates);
+        $(".online-entry:last").append(formattedonlineDate);
+      };
+};
+
+
+
+var work={
+    "jobs":[
+    {
+    "employer":"Travelers Insurance Companies",
+    "title":"Sales Agent",
+    "location":"Knoxville, TN",
+    "dates":"2008 - Present",
+    "description":"I sling policies yo.",
+    "url":"https://www.travelers.com/"
+
+    },{
+    "employer":"The Silent Ballet",
+    "title":"Staff Member",
+    "location":"New York[Online]",
+    "dates":"2010 - 2012",
+    "description":"I interviewed artists and wrote music reviews for this independent e-zine.",
+    "url":"http://www.thesilentballet.com/"
+    },{
+    "employer":"EdFinancial",
+    "title":"Loan Consolidator",
+    "location":"Knoxville, TN",
+    "dates":"2007-2008",
+    "description":"I consolidated hundreds of thousands of dollars of student loans.",
+    "url":"https://www.edfinancial.com/home"
+    },{
+    "employer":"Nova Information Systems",
+    "title":"Voice Authorization Operator",
+    "location":"Knoxville, TN",
+    "dates":"2005 - 2007",
+    "description":"I assited our Spanish-speaking vendors activate their batch of credit cards in merchant services.",
+    "url":"http://www.elavon.com/"
+    }
+    ]
+}
+
+
+work.display = function(){
+    for (job in work.jobs){
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer).replace("#", work.jobs[job].url);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+    var formattedworkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+     $(".work-entry:last").append(formattedworkLocation);
+     var formattedDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+     $(".work-entry:last").append(formattedDescription);
+
+    
+    };
+
+};
+
+
+
+
+
+
+var projects={
+    "projects":[
+    {
+        "title":"Portfolio",
+        "dates":"2014",
+        "description":"Udacity Nanodegree Project 1 Portfolio",
+        "images":["images/meagain.jpg"],
+        "url":"http://unhombremuyhonrado.github.io/v1mockup/"
+    }
+    ]
+}
+
+
+projects.display = function(){
+    for (project in projects.projects){
+        $("#projects").append(HTMLprojectStart);
+
+        var formattedTitle=HTMLprojectTitle.replace("%data%",projects.projects[project].title).replace("#", projects.projects[project].url);
+        $(".project-entry:last").append(formattedTitle);
+
+        var formattedDates=HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+        $(".project-entry:last").append(formattedDates);
+
+        var formattedDescription=HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+        $(".project-entry:last").append(formattedDescription);
+
+        if (projects.projects[project].images.length > 0){
+            for(image in projects.projects[project].images){
+                var formattedImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+
+        }
+
+    }
+}
+
+
+
+
+$(document).click(function(loc){
+	var x = loc.pageX;
+	var y = loc.pageY;
+	logClicks(x,y);
+});
+
+
+
+$("#mapDiv").append(googleMap);
+
+work.display();
+projects.display();
+education.display(); 
+
+
+
